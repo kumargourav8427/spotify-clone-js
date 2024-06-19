f0ea232bcfcf537c748c71c91ca92a230b4e73ad
Gourav <kumargourav8427@gmail.com>
2024-06-19 12:42:41 +0530

    url remove from js

--- js/script.js
+++ js/script.js
@@ -205,6 +205,9 @@ async function main() {
     document.querySelector('.range').getElementsByTagName("input")[0].addEventListener("change", (e) => {
         console.log("Setting volume ", e.target.value, "/100");
         currentSong.volume = parseInt(e.target.value) / 100
+        if (currentSong.volume > 0) {
+            document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg", "volume.svg")
+        }
     })
 
 
