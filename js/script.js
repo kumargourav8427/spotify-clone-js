308b2b608eb5e8b0533f48fc2c112867e4737d4e
Gourav <kumargourav8427@gmail.com>
2024-06-19 12:28:18 +0530

    url remove from js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function convertSecondsToMinutes(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text()
     // console.log(response);
     let div = document.createElement("div")
@@ -78,7 +78,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbhums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -91,7 +91,7 @@ async function displayAlbhums() {
             let folder = (e.href.split("/").slice(-2)[0]);
             // Get the metadata of the folder
 
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json()
             console.log(response);
             cardConatiner.innerHTML = cardConatiner.innerHTML + `
